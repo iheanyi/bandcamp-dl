ec7b0155fa5c6cd20f00365d5402932c3d51e8d6
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2013-04-29 22:10:11 -0400

    Code cleanup.

--- bandcamp-dl.py
+++ bandcamp-dl.py
@@ -1,67 +1,17 @@
-import eyed3
-import json
-from hsaudiotag import auto
-from hsaudiotag import mpeg
 from mutagen.mp3 import MP3
 from mutagen.id3 import ID3, TIT2
 from mutagen.easyid3 import EasyID3
-import simplejson as json
 from shutil import make_archive
-
-import demjson
-import pprint
+from bs4 import BeautifulSoup
 
 import requests
 import os
 import sys
-import zipfile
-import re
 import urllib
-import ast
-
-from bs4 import BeautifulSoup
-
-
-class Result:
-	def __init__(self, url, name, title, which):
-		self.url = url
-		self.title = title
-		self.type = which
-		self.artist = name
-
-	def __str__(self):
-
-		return 
-
-
-
-searchURL = "http://www.bandcamp.com/search?q="
-#def getEyeD3Tags(path):
-results = []
-"""
-	trackInfo = eyed3.load(path)
-	tag = trackInfo.getTag()
-	tag.link(path)
-
-	print "Artist: %s" % tag.getArtist()
-	print "Album: %s" % tag.getAlbum()
-	print "Track: %s" % tag.getTitle()
-	print "Track Length: %s" % trackInfo.getPlayTimeString()
-	print "Release Year: %s" % tag.getYear()"""
-
-def search_artist(query):
-	r = requests.get(searchURL + query)
-	#print r.text
-	parse_results(r.text)
-
-
-def zipdir(path, zip):
-	for root, dirs, files in os.walk(path):
-		for file in files:
-			zip.write(os.path.join(root, file))
-
 
 def parse_file(url):
+
+	print "Starting the parsing for: " + url
 	r = requests.get(url)
 	soup = BeautifulSoup(r.text)
 
@@ -71,13 +21,7 @@ def parse_file(url):
 		songType = "track"
 
 	albumTitle = soup.head.title.text
-
-	#print title
-
-	#regex = re.compile(r'var TralbumData ([^*?])};')
-	#m = regex.search(r.text)
 	
-
 	embedBlock = r.text.split("var EmbedData = ")
 
 	embedStringBlock = embedBlock[1]
@@ -107,9 +51,6 @@ def parse_file(url):
 		fileType = "album"
 
 	albumTitle = embedData['album_title']
-
-
-	print embedData
 
 	block = r.text.split("var TralbumData = ")
 	#print block[0]
@@ -181,20 +122,9 @@ def parse_file(url):
 		os.makedirs("files/" + firstLetter + "/" + artistName)
 
 
-	#pprint.pprint(data)
-	#print data['current']
-
-	#print data['hasAudio']
-	#print data['artFullsizeUrl']
-
-	tracks = data['trackinfo']
-	#print tracks
-	
+	tracks = data['trackinfo']	
 
 	albumPath = albumTitle.replace(" ", "").replace("/","").replace(".", "")
-
-
-
 
 	albumPath = "files/" + firstLetter + "/" + artistName + "/" + albumPath
 	if not os.path.exists("files/zips"):
@@ -203,7 +133,6 @@ def parse_file(url):
 	if not os.path.exists(albumPath):
 		os.makedirs(albumPath)
 
-	#albumPath = "files/" + firstLetter + "/" + artistName + "/" + albumPath
 	for each in tracks:
 		songTitle = each['title'].replace(" ", "").replace(".", "")
 		songURL = each['file']['mp3-128']
@@ -240,30 +169,12 @@ def parse_file(url):
 		#zipdir(albumPath + "/", zip)
 		#zip.close
 
-
-	#print a
-	#print stringBlock
-	#print demjson.decode(stringBlock)
-
-	#print data
-	#data = json.loads(stringBlock.strip())
-
-	#print data['current']
-
-	#print r.text
-		#print "Match found."
-		#print m.group(1)
-	#albumDataBlock = 
-
-
 def parse_results(text):
 	soup = BeautifulSoup(text)
 
 	items = soup.findAll("li", "searchresult")
 
 	for item in items:
-		#print item.text
-
 		typeText = item.find(class_="itemtype").text.strip()
 		albumTitle = item.find(class_="heading").text.strip()
 
@@ -298,16 +209,10 @@ def getTags(path):
 
 
 
-print EasyID3.valid_keys.keys()
 
 url = sys.argv[1]
 if(len(sys.argv) != 2):
 	print "usage: bandcamp-dl.py <url to download>"
-##audio["title"] = unicode("1. yewknxwusup [TWRK]")
-#audio.save()
+	sys.exit
+
 parse_file(url)
-#search_artist("talib kweli")
-#printTags()
-#getMutagenTags("inthecity.mp3")
-#getEyeD3Tags("inthecity.mp3")
-#getTags("inthecity.mp3")
