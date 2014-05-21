5cd095d8800f6d3df6c6ccd22450c0d6251d95c0
Simon W. Jackson <simon@simonwjackson.com>
2014-05-21 15:28:55 +0200

    closes #10 closes #9 closes #8 closes #7 closes #6

--- bandcamp-dl/Bandcamp.py
+++ bandcamp-dl/Bandcamp.py
@@ -8,14 +8,60 @@ from mutagen.easyid3 import EasyID3
 from bs4 import BeautifulSoup
 import requests
 import sys
-import Bandcamp
-from os.path import expanduser
 
 import jsobj
 
 
 class Bandcamp:
-    DOWNLOAD_DIR = expanduser("~/Music")
+
+    def parse(self, url):
+        r = requests.get(url)
+        self.soup = BeautifulSoup(r.text)
+        album = {
+            "tracks": [],
+            "title": "",
+            "artist": "",
+            "full": False,
+            "art": ""
+        }
+
+        album_meta = self.extract_album_meta_data(r)
+
+        album['artist'] = album_meta['artist']
+        album['title'] = album_meta['title']
+
+        for track in album_meta['tracks']:
+            track = self.get_track_meta_data(track)
+            album['tracks'].append(track)
+
+        album['full'] = self.all_tracks_available(album)
+        album['art'] = self.get_album_art()
+
+        return album
+
+
+    def all_tracks_available(self, album):
+        for track in album['tracks']:
+            if track['url'] is None:
+                return False
+
+        return True
+
+
+    def get_track_meta_data(self, track):
+        new_track = {}
+
+        if type(track['file']) is not str:
+            if track['file'].has_key('mp3-128'):
+                new_track['url'] = track['file']['mp3-128']
+        else:
+            new_track['url'] = None
+
+        new_track['duration'] = track['duration']
+        new_track['track'] = track['track_num']
+        new_track['title'] = track['title']
+
+        return new_track
 
 
     def get_embed_string_block(self, request):
@@ -27,105 +73,6 @@ class Bandcamp:
         embedStringBlock = jsobj.read_js_object("var EmbedData = %s" % str(embedStringBlock))
 
         return embedStringBlock
-
-
-    def sanatize_text(self, text, space=False, slash=False, period=False):
-        result = text
-
-        if not space:
-            result = result.replace(" ", "")
-        if not slash:
-            result = result.replace("/", "")
-        if not period:
-            result = result.replace(".", "")
-
-        return result
-
-
-    def download(self, request=None, destination=None, show_progress=False, url=None):
-        if not request:
-            request = urllib2.Request(url)
-
-        remote_file = urllib2.urlopen(request)
-        meta = remote_file.info()
-        file_size = int(meta.getheaders("Content-Length")[0])
-        file_size_dl = 0
-        block_sz = 8192
-
-        local_file = open(destination, 'wb')
-
-        while True:
-            buffer = remote_file.read(block_sz)
-            if not buffer:
-                break
-
-            file_size_dl += len(buffer)
-            local_file.write(buffer)
-
-            if show_progress:
-                self.display_progress(file_size_dl, file_size)
-
-        local_file.close()
-        print "Done downloading: " + destination
-
-
-    def display_progress(self, current, total):
-        factor = float(current) / total
-        percent = factor * 100
-        status = str( int(round(percent)) )
-
-        sys.stdout.write("Download progress: %s%%   \r" % (status))
-        sys.stdout.flush()
-
-
-    def download_track(self, track, url, title, album_path, artist, album):
-        print "Now Downloading: " + track['title'], track['file']['mp3-128'] 
-
-        req = urllib2.Request(url, headers={'User-Agent': "Magic Browser"})
-        destination = album_path + '/' + self.sanatize_text(track['title'], space=True) + '.mp3'
-
-        self.download(req, destination, show_progress = True)
-
-
-    def write_id3_tags(self, track, title, album_path, artist, album):
-        print "Encoding . . . "
-
-        audio = MP3(album_path + '/' + track['title'] + '.mp3')
-        audio["TIT2"] = TIT2(encoding=3, text=["title"])
-        audio.save()
-
-        audio = EasyID3(album_path + '/' + track['title'] + '.mp3')
-        audio["title"] = track['title']
-        audio["artist"] = artist
-        audio["album"] = album
-        audio.save()
-
-        print "Done encoding . . . "
-
-
-    def create_directories(self, artist, album):
-        album_path = self.DOWNLOAD_DIR + "/" + artist + "/" + self.sanatize_text(album)
-
-        if not os.path.exists(self.DOWNLOAD_DIR):
-            os.makedirs(self.DOWNLOAD_DIR)
-
-        if not os.path.exists(self.DOWNLOAD_DIR + "/zips"):
-            os.makedirs(self.DOWNLOAD_DIR + "/zips")
-
-        if not os.path.exists(album_path):
-            os.makedirs(album_path)
-
-        return album_path
-
-        def get_embed_string_block(self, request):
-            embedBlock = request.text.split("var EmbedData = ")
-
-            embedStringBlock = embedBlock[1]
-            embedStringBlock = unicodedata.normalize('NFKD', embedStringBlock).encode('ascii', 'ignore')
-            embedStringBlock = embedStringBlock.split("};")[0] + "};"
-            embedStringBlock = jsobj.read_js_object("var EmbedData = %s" % str(embedStringBlock))
-
-            return embedStringBlock
 
 
     def extract_album_meta_data(self, request):
@@ -152,24 +99,17 @@ class Bandcamp:
         return "http://{0}.bandcamp.com/album/{1}".format(artist, album)
 
 
-    def download_album_art(self):
+    def get_album_art(self):
         url = self.soup.find(id='tralbumArt').find_all('img')[0]['src']
-        filename = self.album['path'] + "/" + "cover.jpg"
-        self.download(destination=filename, url=url)
+        return url
 
-    def parse_album_page(self, url):
-        print "Starting the parsing for: " + url
 
-        r = requests.get(url)
-        self.soup = BeautifulSoup(r.text)
+    def get_embed_string_block(self, request):
+        embedBlock = request.text.split("var EmbedData = ")
 
-        self.album = self.extract_album_meta_data(r)
-        self.album['path'] = self.create_directories(self.album['artist'], self.album['title'])
-        self.download_album_art()
+        embedStringBlock = embedBlock[1]
+        embedStringBlock = unicodedata.normalize('NFKD', embedStringBlock).encode('ascii', 'ignore')
+        embedStringBlock = embedStringBlock.split("};")[0] + "};"
+        embedStringBlock = jsobj.read_js_object("var EmbedData = %s" % str(embedStringBlock))
 
-        for track in self.album['tracks']:
-            title = self.sanatize_text(track['title'], space=True)
-            url = track['file']['mp3-128']
-
-            self.download_track(track, url, title, self.album['path'], self.album['artist'], self.album['title'])
-            self.write_id3_tags(track, title, self.album['path'], self.album['artist'], self.album['title'])
+        return embedStringBlock
--- bandcamp-dl/bandcamp-dl.py
+++ bandcamp-dl/bandcamp-dl.py
@@ -1,18 +1,24 @@
-"""Naval Fate.
+#!/usr/bin/python
+
+"""bandcamp-dl
 Usage:
-  bandcamp-dl.py download [<url> | --artist=<artist> --album=<album>]
+  bandcamp-dl.py <url>
+  bandcamp-dl.py [--template=<template>] [--base-dir=<dir>]
+                 [--full-album]
+                 (<url> | --artist=<artist> --album=<album>)
   bandcamp-dl.py (-h | --help)
-  bandcamp-dl.py --version
+  bandcamp-dl.py (--version)
 
 Options:
-  -h --help     Show this screen.
-  --version     Show version.
-  --artist=<artist>  The artist's slug (from the URL)
-  --album=<album>  The album's slug (from the URL)
+  -h --help                 Show this screen.
+  -v --version              Show version.
+  -a --artist=<artist>      The artist's slug (from the URL)
+  -b --album=<album>        The album's slug (from the URL)
+  -t --template=<template>  Output filename template.
+                            [default: %{artist}/%{album}/%{track} - %{title}]
+  -d --base-dir=<dir>       Base location of which all files are downloaded
+  -f --full-album           Download only if all tracks are availiable
 """
-
-from docopt import docopt
-from Bandcamp import Bandcamp
 
 """ Coded by:
 
@@ -30,16 +36,39 @@ Iheanyi:
     Shout out to darkf for writing a helper function for parsing the JavaScript! """
 
 
+from docopt import docopt
+from Bandcamp import Bandcamp
+from BandcampDownloader import BandcampDownloader
+import os
+
+
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='bandcamp-dl 1.0')
     bandcamp = Bandcamp()
 
-    print arguments
-
-    if (arguments['download']):
+    if (arguments):
         if (arguments['--artist'] and arguments['--album']):
-            url = Bandcamp.generate_album_url(arguments['--artist'], arguments['--album']) 
-            bandcamp.parse_album_page(url)
+            url = Bandcamp.generate_album_url(arguments['--artist'], arguments['--album'])
 
-        elif (arguments['<url>']):
-            bandcamp.parse_album_page(arguments['<url>'])
+        elif arguments['<url>']:
+            url = arguments['<url>']
+
+        if url:
+            album = bandcamp.parse(url)
+
+            if arguments['--base-dir']:
+                basedir = arguments['--base-dir']
+            else:
+                basedir = os.path.dirname(os.path.realpath(__file__))
+
+            if arguments['--template']:
+                template = arguments['--template']
+
+            if arguments['--full-album'] and not album['full']:
+                print "Full album not availiable. Skipping.."
+            else:
+                bandcamp_downloader = BandcampDownloader(url, template, basedir)
+                bandcamp_downloader.start(album)
+
+    else:
+        print __doc__
