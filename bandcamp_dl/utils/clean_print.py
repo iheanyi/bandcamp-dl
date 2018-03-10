243e53768410bd4051002b9e30e7366328b9ad03
Anthony Forsberg <forsberganthony@yahoo.com>
2018-03-09 20:24:52 -0500

    Switch to shutil for getting terminal size
    Due to possible permissions issues with automated testing switching to shutil to indirectly call os.get_terminal_size() is best.

--- bandcamp_dl/utils/clean_print.py
+++ bandcamp_dl/utils/clean_print.py
@@ -1,7 +1,7 @@
-import os
+import shutil
 
 
 def print_clean(msg):
-    terminal_size = os.get_terminal_size()
+    terminal_size = shutil.get_terminal_size()
     msg_length = len(msg)
     print("{}{}".format(msg, " " * (int(terminal_size[0]) - msg_length)), end='')
