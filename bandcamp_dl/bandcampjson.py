6d4bbb7f29650fb65f25bcc993af5f7876fe9d19
Piotr Patrzyk <piotr@patrzyk.me>
2020-09-24 15:17:36 +0200

    rm extract_data

--- bandcamp_dl/bandcampjson.py
+++ bandcamp_dl/bandcampjson.py
@@ -16,8 +16,6 @@ class BandcampJSON:
         """Grabbing needed data from the page"""
         self.get_pagedata()
         self.get_js()
-        self.regex = re.compile(r"(?<=var\s" + target + "\s=\s).*?(?=};)", re.DOTALL)
-        self.target = target
         self.js_to_json()
         return self.json_data
 
@@ -29,15 +27,7 @@ class BandcampJSON:
     def get_js(self):
         """Get <script> element containing the data we need and return the raw JS"""
         logging.debug("Grabbing embedded script..")
-        self.js_data = self.body.find("script", {"type": "application/json+ld"}, text=re.compile(self.target)).string
-        self.extract_data(self.js_data)
-
-    def extract_data(self, js: str):
-        """Extract values from JS dictionary
-
-        :param js: Raw JS
-        """
-        self.js_data = self.regex.search(js).group().replace('" + "', '') + "}"
+        self.js_data = self.body.find("script", {"type": "application/json+ld"}).string
 
     def js_to_json(self):
         """Convert JavaScript dictionary to JSON"""
