13fe80f23bacfca1ffeeff926b19b2249682ce73
Piotr Patrzyk <piotr@patrzyk.me>
2020-09-24 15:05:48 +0200

    bandcampjson simplify

--- bandcamp_dl/bandcampjson.py
+++ bandcamp_dl/bandcampjson.py
@@ -7,33 +7,28 @@ import demjson
 class BandcampJSON:
     def __init__(self, body, debugging: bool=False):
         self.body = body
-        self.targets = ['TralbumData', 'EmbedData', 'pagedata']
-        self.json_data = []
+        self.json_data = dict()
 
         if debugging:
             logging.basicConfig(level=logging.DEBUG)
 
-    def generate(self) -> list:
-        """Iterate through targets grabbing needed data"""
-        for target in self.targets:
-            if target[:4] == 'page':
-                self.get_pagedata()
-            else:
-                logging.debug(" Grabbing target data..")
-                self.regex = re.compile(r"(?<=var\s" + target + "\s=\s).*?(?=};)", re.DOTALL)
-                self.target = target
-                self.js_to_json()
+    def generate(self):
+        """Grabbing needed data from the page"""
+        self.get_pagedata()
+        self.get_js()
+        self.regex = re.compile(r"(?<=var\s" + target + "\s=\s).*?(?=};)", re.DOTALL)
+        self.target = target
+        self.js_to_json()
         return self.json_data
 
     def get_pagedata(self):
-        """Grab bandcamp pagedata JSON"""
+        logging.debug("Grab pagedata JSON..")
         pagedata = self.body.find('div', {'id': 'pagedata'})['data-blob']
-        # Add pagedata to the list of JSON strings
-        self.json_data.append(pagedata)
+        self.json_data['pagedata'] = pagedata
 
     def get_js(self):
         """Get <script> element containing the data we need and return the raw JS"""
-        logging.debug(" Grabbing embedded script..")
+        logging.debug("Grabbing embedded script..")
         self.js_data = self.body.find("script", {"src": False}, text=re.compile(self.target)).string
         self.extract_data(self.js_data)
 
@@ -47,8 +42,7 @@ class BandcampJSON:
     def js_to_json(self):
         """Convert JavaScript dictionary to JSON"""
         logging.debug(" Converting JS to JSON..")
-        self.get_js()
         # Decode with demjson first to reformat keys and lists
         decoded_js = demjson.decode(self.js_data)
         # Encode to make valid JSON, add to list of JSON strings
-        self.json_data.append(demjson.encode(decoded_js))
+        self.json_data['target'] = demjson.encode(decoded_js)
