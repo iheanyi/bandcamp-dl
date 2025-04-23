f8a432c4bec4e914a64e66b82e64c54fb10ab345
Pete Beardmore <pete.beardmore@msn.com>
2025-04-23 18:02:36 +0100

    Config, make update 'private'
    - Nothing external uses the 'update_with_dict()', so keep the pattern
      of '_'-prefix consistent

--- bandcamp_dl/config.py
+++ bandcamp_dl/config.py
@@ -52,7 +52,7 @@ class Config(dict):
                     # change hyphen with undersore
                     user_config = {k.replace('-', '_'): v for k, v in user_config.items()}
                     # overwrite defaults with user provided config
-                    self.update_with_dict(user_config)
+                    self._update_with_dict(user_config)
                 except json.JSONDecodeError:
                     # NOTE: we don't have logger yet
                     sys.stderr.write(f"Malformed configuration file `{CONFIG_PATH}'. Check json syntax.\n")
@@ -63,7 +63,7 @@ class Config(dict):
                 json.dump(conf, fobj)
             sys.stderr.write(f"Configuration has been written to `{CONFIG_PATH}'.\n")
 
-    def update_with_dict(self, dict_):
+    def _update_with_dict(self, dict_):
         for key, val in dict_.items():
             if key not in self:
                 continue
